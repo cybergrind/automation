2ba3b9d5c2638ca3fa5c335ce1d658e072cd7e71
Kirill Pinchuk <cybergrind@gmail.com>
2024-05-21 12:46:38 +0300

    soulrend support

--- capture/games/poe2.py
+++ capture/games/poe2.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import logging
+import threading
 import time
+from dataclasses import dataclass
 from subprocess import run
+from uuid import uuid4
 
-import numpy as np
 import pyautogui
-from system_hotkey import SystemHotkey
-
 from fan_tools.python import rel_path
+from pynput import mouse
+from system_hotkey import SystemHotkey
 
 from capture.common import crop
 from capture.cv import imread, match
 from capture.types import Rect
-from capture.utils import ctx, dtime, spell, throttle
+from capture.utils import ctx
 
 
 T_DIR = rel_path('../../templates/poe')
@@ -23,6 +25,7 @@ KB_NEW_INSTANCE = ('kp_next',)
 KB_WP_CLICK = ('kp_down',)
 KP_UP = ('kp_up',)
 KP_END = ('kp_end',)
+KP_LEFT = ('kp_left',)  # 6
 WP_IMAGE = imread(T_DIR / 'wp.png')
 RESS_IMAGE = imread(T_DIR / 'ress.png')
 DEL_IMAGE = imread(T_DIR / 'delir.png')
@@ -88,6 +91,85 @@ def click_waypoint():
         log.info('no wp found')
 
 
+CLICKS_UUID = uuid4()
+
+
+def register_clicks():
+    global CLICKS_UUID
+    our_uuid = uuid4()
+    CLICKS_UUID = our_uuid
+
+    def clicks():
+        if CLICKS_UUID != our_uuid:
+            # long press repeated uuids
+            return
+
+        hk.unregister(KP_END)
+        with pyautogui.hold('ctrl'):
+            for _ in range(8):
+                pyautogui.click()
+                time.sleep(0.01)
+        register_clicks()
+
+    hk.register(KP_END, callback=lambda x: clicks(), overwrite=True)
+
+
+def ensure_press(key, delay=0.23):
+    """
+    variants:
+    pynput
+    python-evdev
+    uinput
+
+    xdotool
+    """
+    with pyautogui.hold(key, _pause=True):
+        time.sleep(delay)
+    return
+
+
+@dataclass
+class SoulrendLoop:
+    enabled: bool = False
+    active: bool = False
+    last_life: float = 0
+    last_malevolence: float = 0
+    iteration: int = 0
+    exit: bool = False
+
+
+SOULREND_LOOP = SoulrendLoop()
+LIFE_TICK = 4.7
+MALEVOLENCE_TICK = 18.5
+
+
+def run_soulrend_loop():
+    while not SOULREND_LOOP.exit:
+        time.sleep(0.1)
+        SOULREND_LOOP.iteration += 1
+        if SOULREND_LOOP.iteration < 8:
+            continue
+        if not SOULREND_LOOP.enabled:
+            continue
+        if not SOULREND_LOOP.active:
+           continue
+        t = time.time()
+        if SOULREND_LOOP.last_life + LIFE_TICK < t:
+            log.debug('press t')
+            ensure_press('t')
+            SOULREND_LOOP.last_life = t
+        elif SOULREND_LOOP.last_malevolence + MALEVOLENCE_TICK < t:
+            log.debug('press a')
+            ensure_press('a')
+            SOULREND_LOOP.last_malevolence = t
+
+
+def pressed_soulrend_loop(param):
+    log.debug(f'{param=}')
+    SOULREND_LOOP.enabled = not SOULREND_LOOP.enabled
+    SOULREND_LOOP.iteration = 0
+
+
 def get_single():
     # shift + click, then enter
     with pyautogui.hold('shift'):
@@ -96,13 +178,9 @@ def get_single():
     pyautogui.press('enter')
 
 
-def clicks():
-    hk.unregister(KP_END)
-    with pyautogui.hold('ctrl'):
-        for _ in range(8):
-            pyautogui.click()
-            time.sleep(0.01)
-    hk.register(KP_END, callback=lambda x: clicks(), overwrite=True)
+def mouse_click(x, y, button, pressed):
+    if button == mouse.Button.right:
+        SOULREND_LOOP.active = pressed
 
 
 def main():
@@ -110,11 +188,20 @@ def main():
     hk.register(KB_NEW_INSTANCE, callback=lambda x: go_instance(), overwrite=True)
     hk.register(KB_WP_CLICK, callback=lambda x: click_waypoint(), overwrite=True)
     hk.register(KP_UP, callback=lambda x: get_single(), overwrite=True)
-    hk.register(KP_END, callback=lambda x: clicks(), overwrite=True)
-
-    while True:
-        time.sleep(0.1)
-        # log.debug(pyautogui.position())
+    sl_loop = threading.Thread(target=run_soulrend_loop)
+    sl_loop.start()
+    hk.register(KP_LEFT, callback=lambda x: pressed_soulrend_loop(x), overwrite=True)
+    # hk.register(KP_END, callback=lambda x: clicks(), overwrite=True)
+    register_clicks()
+    mouse_listener = mouse.Listener(on_click=mouse_click)
+    mouse_listener.start()
+    try:
+        while True:
+            time.sleep(0.1)
+            # log.debug(pyautogui.position())
+    except:
+        SOULREND_LOOP.exit = True
+        mouse_listener.stop()
 
 
 if __name__ == '__main__':
